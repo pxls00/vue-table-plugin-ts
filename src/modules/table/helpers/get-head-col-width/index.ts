49b483650e1f9e685cffc10b3adfaae5b8d05f6f
Abdurahim <89141756+pxls00@users.noreply.github.com>
2023-06-09 20:57:57 +0500

    structured table-main-component

---
+++ src/modules/table/helpers/get-head-col-width/index.ts
@@ -0,0 +1,130 @@
+import generateHeadWidth from './get-head-col-width'
+import type {IHeadItemBase, TTableHeadGroupedRow} from '../../index.types'
+
+class HeadColsWidth {
+  private _columnsWithoutWidth: IHeadItemBase[] = []
+  private _overWidth: number
+  private _columnsData: IHeadItemBase[] = []
+  private _tableWidth: number
+  private _columnsRowSpreated: IHeadItemBase[] = []
+  private _columnsRowGrouped: TTableHeadGroupedRow[] = []
+
+  get columnsWithoutWidth () {
+    return this._columnsWithoutWidth
+  }
+
+  get overWidth () {
+    return this._overWidth
+  }
+
+  get columnsData () {
+    return this._columnsData
+  }
+
+  get tableWidth () {
+    return this._tableWidth
+  }
+
+  get columnsRowSpreated () {
+    return this._columnsRowSpreated
+  }
+
+  get columnsRowGrouped () {
+    return this._columnsRowGrouped
+  }
+
+  constructor (
+    overWidth: number,
+    columnsData: IHeadItemBase[],
+    columnsRowSpreated: IHeadItemBase[],
+    columnsRowGrouped: TTableHeadGroupedRow[]
+  ) {
+    this._tableWidth = overWidth
+    this._overWidth = this._tableWidth
+    this._columnsData = columnsData
+    this._columnsRowSpreated = columnsRowSpreated
+    this._columnsRowGrouped = columnsRowGrouped
+  }
+
+  setHeadColsWidth (
+    data: IHeadItemBase[] = this._columnsData,
+    maxWidth: number = this._tableWidth
+  ) {
+    this.setHeadColsWidthIfTheyExists(data, maxWidth)
+
+    if (this._columnsWithoutWidth.length) {
+      this.setHeadColsWidthIfTheyNotExists(data)
+    }
+  }
+
+  setHeadColsWidthIfTheyExists (data: IHeadItemBase[], maxWidth: number): void {
+    data.forEach((item) => {
+      if (!item.width) {
+        this._columnsWithoutWidth.push(item)
+      } else {
+        item.width = this.getHeadColWidth(item.width, item.minWidth, maxWidth)
+      }
+    })
+  }
+
+  setHeadColsWidthIfTheyNotExists (data: IHeadItemBase[]): void {
+    data.forEach((item) => {
+      if (item.width && typeof item.width === 'number') {
+        this._overWidth -= item.width
+      }
+    })
+
+    if (this._overWidth > 0) {
+      data.forEach((item: IHeadItemBase) => {
+        if (!item.width) {
+          item.width = (this._overWidth - 10) / this._columnsWithoutWidth.length
+        }
+
+        if (item.children && item.children.length) {
+          this.setHeadColsWidth(item.children, item.width as number)
+        }
+      })
+    }
+  }
+
+  setHeadColsGrouping (
+    data: IHeadItemBase[] = this._columnsData,
+    index: number = 0
+  ): void {
+    const indexColumnRow: number = index
+      ? index
+      : this._columnsRowGrouped.length
+
+    if (
+      !this._columnsRowGrouped[indexColumnRow] ||
+      !this._columnsRowGrouped[indexColumnRow].length
+    ) {
+      this._columnsRowGrouped[indexColumnRow] = []
+    }
+
+    data.forEach((item: IHeadItemBase) => {
+      const _ = JSON.parse(JSON.stringify(item))
+
+      delete _.children
+      this._columnsRowGrouped[indexColumnRow].push(_)
+
+      if (item.children && item.children.length) {
+        this.setHeadColsGrouping(item.children, indexColumnRow + 1)
+      }
+    })
+  }
+
+  setHeadColsSpread (data: IHeadItemBase[] = this._columnsData): void {
+    data.forEach((item: IHeadItemBase) => {
+      if (item.children && item.children.length) {
+        this.setHeadColsSpread(item.children)
+      } else if (!item.children) {
+        this._columnsRowSpreated.push(item)
+      }
+    })
+  }
+
+  getHeadColWidth = generateHeadWidth
+}
+
+export default HeadColsWidth
---
+++ src/modules/table/helpers/mouse-move-resizer/index.ts
@@ -0,0 +1,108 @@
+import type {IHeadItemBase, IResizeData} from '../../index.types'
+
+class MouseMoveResizer {
+  private _resizing: boolean = false
+  private _resizeIndex: number = 0
+  private _resizeEL: HTMLSpanElement | undefined
+  private _resizeNum: number = 0
+  private _startX: number = 0
+  private _columnsData: IHeadItemBase[] = []
+
+  get resizing (): boolean {
+    return this._resizing
+  }
+
+  get resizeIndex (): number {
+    return this._resizeIndex
+  }
+
+  get startX (): number {
+    return this._startX
+  }
+
+  get columnsData (): IHeadItemBase[] {
+    return this._columnsData
+  }
+
+  constructor (columnsData: IHeadItemBase[]) {
+    this._columnsData = columnsData
+  }
+
+  startResize ({ index, el }: IResizeData) {
+    const eventTarget = event as MouseEvent
+
+    this._resizeEL = el
+    this._resizing = true
+    this._resizeIndex = index
+    this._startX = eventTarget.pageX
+  }
+
+  stopResize () {
+    this.resizeColumns(this._resizeNum)
+
+    if (this._resizeEL) {
+      this._resizeEL.style.right = '0px'
+    }
+
+    this._resizing = false
+    this._resizeIndex = 0
+    this._startX = 0
+  }
+
+  handleMouseMove (event: Event | any): void {
+    if (this._resizing) {
+      const deltaX: number = event.pageX - this.startX
+
+      if (this._resizeEL) {
+        const buttonRight = this._resizeEL.style.right
+
+        this._resizeNum = +buttonRight.replace('px', '') + deltaX * -1
+        this._resizeEL.style.right = `${this._resizeNum}px`
+      }
+      // const currentParallelColumn = this._columnsData[this._resizeIndex + 1]
+      // const newColumnWidth: number = (currentColumn.width as number) + deltaX
+      // const newParralelColumnWidth =
+      //   (currentParallelColumn.width as number) + deltaX * -1
+
+      // if (
+      //   newColumnWidth >= currentColumn.minWidth &&
+      //   newParralelColumnWidth >= currentParallelColumn.minWidth
+      // ) {
+      //   currentColumn.width = newColumnWidth
+      //   currentParallelColumn.width = newParralelColumnWidth
+      // }
+      // this.resizeColumns(deltaX)
+      this._startX = event.pageX
+    }
+  }
+
+  resizeColumns (widthNumber: number): void {
+    const currentColumn: IHeadItemBase =
+      this._columnsData[this._resizeIndex]
+    const currentParallelColumn = this._columnsData[this._resizeIndex + 1]
+    const newColumnWidth: number =
+      (currentColumn.width as number) + widthNumber * -1
+    const newParralelColumnWidth =
+      (currentParallelColumn.width as number) + widthNumber
+    const canGetWidth =
+      (currentParallelColumn.width as number) - currentParallelColumn.minWidth
+    const newColumnWidthWithCanGetWidth =
+      (currentColumn.width as number) + canGetWidth
+
+    if (
+      newColumnWidth >= currentColumn.minWidth &&
+      newParralelColumnWidth >= currentParallelColumn.minWidth &&
+      newColumnWidth < newColumnWidthWithCanGetWidth
+    ) {
+      currentParallelColumn.width = newParralelColumnWidth
+      currentColumn.width = newColumnWidth
+    }
+
+    if (newColumnWidth > newColumnWidthWithCanGetWidth) {
+      currentParallelColumn.width = currentParallelColumn.minWidth
+      currentColumn.width = newColumnWidthWithCanGetWidth
+    }
+  }
+}
+
+export default MouseMoveResizer
